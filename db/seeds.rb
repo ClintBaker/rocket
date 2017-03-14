1fe4c4f0cd35be9a91db0bdb2b0cc68fecb620e2
Clint Baker <clintonjamesbaker@gmail.com>
2017-03-13 22:30:55 -0600

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+require 'csv'
+CSV.foreach(Rails.root.join("db/seeds_data/product.csv"), headers: true) do |row|
+  Product.find_or_create_by(title: row[0], price: row[1], description: row[2])
+end
+
+
+
+
+
+
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
